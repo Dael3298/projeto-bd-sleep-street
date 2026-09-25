c7ac4b31d114bd8a2ab20ad1c9f9d641f27651ea
Dhaniel Moreira <dhaniel.moreira@escola.pr.gov.br>
2026-09-25 11:31:57 -0300

    Adicionei a tabela historico_status_reembolso

--- script-bd-sleep-street.sql
+++ script-bd-sleep-street.sql
@@ -8,12 +8,31 @@ CREATE TABLE estabelecimento_favorito(
 CREATE TABLE estabelecimento(
   id_estabelecimento SERIAL PRIMARY KEY,
   nome varchar(150) note null,
-  categoria categoria_estabelecimento_enum varchar note null,
-  faixa_preco faixa_preco_enum varchar note null,
+  categoria categoria_estabelecimento_enum varchar not null,
+  faixa_preco faixa_preco_enum varchar not null,
   avaliacao_media decimal(3,2),
-  latitude decimal(10,8) note null,
-  longitude decimal(11,8) note null,
+  latitude decimal(10,8) not null,
+  longitude decimal(11,8) not null,
   horario_abertura time,
   horario_fechamento time,
   ativo boolean
-)
+);
+
+CREATE TABLE despesa_viagem(
+  id_despesa SERIAL PRIMARY KEY,
+  id_viagem int not null,
+  categoria categoria_despesa_enum varchar not null,
+  descricao varchar(150) not null,
+  valor_despesa decimal(10,2) not null,
+  data_despesa timestamp
+);
+
+CREATE TABLE historico_status_reembolso(
+  id_historico SERIAL PRIMARY KEY,
+  id_reembolso int not null,
+  status_anterior status_reembolso_enum varchar,
+  status_novo status_reembolso_enum varchar not null,
+  observacao_moderador text,
+  id_modificador int not null,
+  data_modificacao timestamp
+);
